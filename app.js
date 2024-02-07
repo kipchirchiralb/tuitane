2ff516fa07da24200511a110899ce11d461c6193
albert Kipchirchir <albertkipchirchir02@gmail.com>
2024-02-07 21:23:35 +0300

    pages set up

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const mysql = require("mysql");
+const dbconn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_tuitane",
+});
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  dbconn.query("SELECT * FROM posts", (err, posts) => {
+    if (err) return res.send(`Error Occured  ${err.message}`);
+    res.render("home.ejs", { posts });
+  });
 });
-//other routes/url paths
+app.get("/profile", (req, res) => {
+  res.render("profile.ejs");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
 app.listen(8000, () => console.log("App running on localhost:8000"));
